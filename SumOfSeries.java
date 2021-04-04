ea9da96bef396cd7eb788922995cee944113e973
Nalin Bisht <53000521+nalinbisht@users.noreply.github.com>
2021-04-04 16:59:50 +0530

    Add files via upload

---
+++ SumOfSeries.java
@@ -0,0 +1,16 @@
+//Series: 1 + 1/2 + 1/3 + 1/4 + ....... + 1/n
+
+import java.util.Scanner;
+
+public class SumOfSeries {
+    public static void main(String []args){
+        System.out.print("Enter value of n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float Sum = 0;
+        for(float i = 1 ; i <=n ; i++){
+            Sum += 1/i;
+        }
+        System.out.println("Sum of Series is: " + Sum);
+    }
+}
