0a4c728a342e0fe309f2fb44800f6cb69b88deca
Nalin Bisht <53000521+nalinbisht@users.noreply.github.com>
2021-04-04 20:29:12 +0530

    Add files via upload

---
+++ SumOfSeries2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumOfSeries2 {
+    public static void main(String []args){
+        System.out.print("Enter value of n: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        float Sum=0;
+        for(float i=1; i<=n ; i++){
+            if(i%2 == 0)
+                Sum -= 1/i;
+            else
+                Sum += 1/i;
+        }
+        System.out.println("Sum of Series is: "+ Sum);
+    }
+}
