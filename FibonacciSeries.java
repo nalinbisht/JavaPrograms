aed19bcdbc0cdac19ee6b8315b4fb098d89c6c05
Nalin Bisht <53000521+nalinbisht@users.noreply.github.com>
2021-04-04 15:59:08 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,22 @@
+//program to print first n elements 6of Fibonacci Series
+import java.util.Scanner;
+public class FibonacciSeries {
+    public static void main(String []args){
+        System.out.print("Enter value of n ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        System.out.print(a+" "+b+" ");
+
+        for(int i=0 ; i<n-2 ; i++){
+            c = a + b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+    }
+}
