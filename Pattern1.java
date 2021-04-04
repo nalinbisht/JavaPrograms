e00004c8b2088fac629fd3b2cec9827da0929f3b
Nalin Bisht <53000521+nalinbisht@users.noreply.github.com>
2021-04-04 21:21:50 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String []args){
+        System.out.print("Enter value of n: ");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1; j<=n ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+Pattern
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
